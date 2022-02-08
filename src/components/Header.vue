94d7a72d188a77d4bd4ef53098a9a8b650f37d2a
Adner16 <giovanni.renda16@gmail.com>
2022-02-08 16:49:56 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,36 @@
 <template>
   <header>
-      <figure>
+      <div class="container">
+           <figure>
           <img src="../assets/img/dc-logo.png" alt="">
       </figure>
       <ul>
-          <li></li>
+          <li>
+              <a href="#">CHARACTERS</a>
+          </li>
+          <li class="active">
+              <a href="#">COMICS</a>
+          </li><li>
+              <a href="#">MOVIES</a>
+          </li><li>
+              <a href="#">TV</a>
+          </li><li>
+              <a href="#">GAMES</a>
+          </li><li>
+              <a href="#">COLLECTIBLES</a>
+          </li><li>
+              <a href="#">VIDEO</a>
+          </li><li>
+              <a href="#">FANS</a>
+          </li><li>
+              <a href="#">NEWS</a>
+          </li><li>
+              <a href="#">SHOP</a>
+          </li>
+
       </ul>
+      </div>
+     
   </header>
 </template>
 
@@ -16,6 +41,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss'>
+    header .container{
+        width: 1200px;
+        margin: auto;
+        display: flex;
+        justify-content: space-between;
+        padding: 10px;
 
+        ul{
+            display: flex;
+            align-items: center;
+
+            li{
+                margin: 5px;
+                list-style-type: none;
+                a{
+                    color: black;
+                    text-decoration: none;
+                }
+            }
+        }
+    }
 </style>
