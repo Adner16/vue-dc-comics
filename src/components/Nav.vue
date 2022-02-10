01ffa72430c94a4f04767383d44627d1eebbde36
Adner16 <giovanni.renda16@gmail.com>
2022-02-10 15:15:11 +0100

    added data

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="background">
         <div class="container">
-            <ul>
+            <ul v-for="card in cards" :key="card.text" class="card-img">
                 <li>
                     <figure>
-                        <img src="../assets/img/buy-comics-digital-comics.png" alt="icon">
+                        <img 
+                            :src="require(`../assets/img/${card.url}`)" 
+                            :alt="card.description"
+                        />
                     </figure>
-                    <span>DIGITAL COMICS</span>
+                    <span class="text">{{ card.text }}</span>
                 </li>
-                <li>
-                    <figure>
+                <!-- <li> -->
+                    <!-- <figure>
                         <img src="../assets/img/buy-comics-merchandise.png" alt="icon">
                     </figure>
                     <span>DC MERCHANDISE</span></li>
@@ -29,17 +32,47 @@
                         <img src="../assets/img/buy-dc-power-visa.svg" alt="logo">
                     </figure>
                     <span>DC POWER VISA</span>
-                </li>
+                </li> -->
             </ul>
-  </div>
-    </div>
-  
+        </div>
+    </div>  
 </template>
 
 <script>
 export default {
     name: 'Nav',
-}
+    data() {
+        return {
+            cards:[
+                {
+                    text: 'DIGITAL COMICS',
+                    url: 'buy-comics-digital-comics.png',
+                    description: 'logo dc',
+                },
+                {
+                    text: 'DC MERCHANDISE',
+                    url: 'buy-comics-merchandise.png,',
+                    description: 'logo merchandise',
+                },
+                {
+                    text: 'SUBSCRIPTION',
+                    url: 'buy-comics-subscription.png',
+                    description: 'logo subscription',
+                },
+                {
+                    text: 'COMIC SHOP LOCATOR',
+                    url: 'buy-comics-shop-locator.png',
+                    description: 'logo shop',
+                },
+                {
+                    text: 'DC POWER VISA',
+                    url: 'buy-dc-power-visa',
+                    description: 'logo visa',
+                },
+            ],
+        };
+    },
+};
 </script>
 
 <style scoped lang='scss'>
